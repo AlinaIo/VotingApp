2f8ecd41bb557664ab522aaf40a5e375e93414f0
IonescuA <alina.ionescu@haufe-lexware.com>
2018-05-12 02:18:53 +0300

    added charts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import OurPieChart from './components/piechart';
+import OurBarChart from './components/barchart';
 
 class App extends Component {
   render() {
@@ -10,10 +12,18 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+          <OurPieChart />
+          {/* <OurBarChart /> */}
+        </div>
+        <div className="loader">
+          <h1>Water</h1>
+        </div>
       </div>
+
     );
   }
 }
---
+++ src/components/piechart.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { PieChart } from 'react-easy-chart';
+
+class OurPieChart extends Component {
+    render() {
+        return (
+            <PieChart
+                size={400}
+                innerHoleSize={200}
+                labels
+                data={[
+                    { key: 'A', value: 100, color: '#aaac84' },
+                    { key: 'B', value: 200, color: '#dce7c5' },
+                    { key: 'C', value: 50, color: '#e3a51a' }
+                ]}
+                styles={{
+                    '.chart_text': {
+                        fontSize: '1em',
+                        fill: '#fff'
+                    }
+                }}
+            />)
+    }
+}
+
+export default OurPieChart;
