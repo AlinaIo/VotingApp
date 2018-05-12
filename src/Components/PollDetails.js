ddccb658c030269b648bf94019d588fa9ea17e5d
Diana Panta <diana-maria.panta@haufe-lexware.com>
2018-05-12 16:51:20 +0300

    list props

--- src/Components/PollDetails.js
+++ src/Components/PollDetails.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
-import { Button } from 'react-bootstrap';
+import { Button } from "react-bootstrap";
+import "./PostDetail.css";
 
 class PollDetails extends Component {
   render() {
-
+    const buttons = this.props.topic.options.map(function(item, i) {
+      console.log("test");
+      return (
+        <Button
+          key={i}
+          bsStyle="success"
+          className="btn btn-primary addTopicPanel-margins"
+        >
+          {item}
+        </Button>
+      );
+    });
     return (
       <div>
-        <Button bsStyle="success" className="btn btn-primary addTopicPanel-margins">Yes</Button>
-        <Button bsStyle="danger" className="btn btn-primary addTopicPanel-margins">No</Button>
+        <h2>Your voting options</h2>
+        <div className="option">{buttons}</div>
       </div>
     );
   }
