abc21be46ce1b96fcf47f96e8de2b89c539f12ad
Patricia Petrut <petrut.patricia@gmail.com>
2018-05-12 14:26:02 +0300

    buttons

--- src/Components/TopicListItem.js
+++ src/Components/TopicListItem.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./AddTopicPanel.css";
+import { PollDetails } from "./PollDetails"
 
 class TopicListItem extends React.Component {
   state = {
-    open: false
+    open: false,
+    display: 'none'
   };
 
   render() {
@@ -12,17 +14,19 @@ class TopicListItem extends React.Component {
         className="list-item"
         onClick={() => {
           console.log("supa");
-          this.setState({ open: !this.state.open });
+          this.setState({ open: !this.state.open ? this.state.display : this.state.display = "inline" });
+
         }}
       >
         <i
           className={`fas fa-chevron-circle-down arrow ${
             this.state.open ? "arrow-rotate" : ""
-          }`}
+            }`}
         />
         <li className="list-group-item list-group-item-info text-item">
           {this.props.topic.name}
         </li>
+        <PollDetails style={{ display: this.state.display }} />
       </div>
     );
   }
