64e414bed683ed7f7fd154bf33895602c2cbbc76
Boc <radu.boc@haufe-lexware.com>
2018-05-12 03:02:53 +0300

    merge

--- src/components/barchart.js
+++ src/components/barchart.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import { BarChart } from 'react-easy-chart';
+// import React, { Component } from 'react';
+// import { BarChart } from 'react-easy-chart';
 
-class OurBarChart extends Component {
-    render() {
-        return (
-            <BarChart
-                data={[
-                    { x: 'A', y: 20 },
-                    { x: 'B', y: 30, color: '#f00' },
-                    { x: 'C', y: 40 },
-                    { x: 'D', y: 20 },
-                    { x: 'E', y: 40 },
-                    { x: 'F', y: 25 },
-                    { x: 'G', y: 5, color: 'orange' }
-                ]}
-            />)
-    }
-}
+// class OurBarChart extends Component {
+//     render() {
+//         return (
+//             <BarChart
+//                 data={[
+//                     { x: 'A', y: 20 },
+//                     { x: 'B', y: 30, color: '#f00' },
+//                     { x: 'C', y: 40 },
+//                     { x: 'D', y: 20 },
+//                     { x: 'E', y: 40 },
+//                     { x: 'F', y: 25 },
+//                     { x: 'G', y: 5, color: 'orange' }
+//                 ]}
+//             />)
+//     }
+// }
 
-export default OurBarChart;
+// export default OurBarChart;
