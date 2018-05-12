3502362ad90aab274c09bede577c4294c3c43e25
IonescuA <alina.ionescu@haufe-lexware.com>
2018-05-12 20:17:05 +0300

    added data from backend to chart

--- src/Components/ChartsPage.js
+++ src/Components/ChartsPage.js
@@ -1,31 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import { OurLineChart } from './Linechart';
 import { OurPieChart } from './Piechart';
 
-const ChartsPage = () => (
-    <div>
-        <div className="charts-header">
-            <h1>Do you want to save the world?</h1>
-        </div>
-        <div className="flex-grid-thirds">
-            <button className="button">Yes</button>
-            <button className="button">No</button>
-            <button className="button">I don't care</button>
-        </div>
-        <div className="flex-grid">
-            <div className="col">
+class ChartsPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            results: {}
+        }
+    }
+    componentDidMount(){
+        fetch('http://backend-hacktm-2018.westeurope.cloudapp.azure.com:3001/results/2')
+            .then(response => {
+                return response.json();
+            })
+            .then((myres) => {
+                this.setState({results:myres})
+            })
+    }
+    render() {
+        return (
+            <div>
+                <div className="charts-header">
+                    <h1>Do you want to save the world?</h1>
+                </div>
+                <div className="flex-grid-thirds">
+                    <button className="button">Yes</button>
+                    <button className="button">No</button>
+                    <button className="button">I don't care</button>
+                </div>
+                <div className="flex-grid">
+                    <div className="col">
+                    </div>
+                    <div className="col">
+                        <OurPieChart voteresults={this.state.results}/>
+                    </div>
+                    <div className="col">
+                        <OurLineChart />
+                    </div>
+                    <div className="col">
+                    </div>
+                </div>
             </div>
-            <div className="col">
-                <OurPieChart />
-            </div>
-            <div className="col">
-                <OurLineChart />
-            </div>
-            <div className="col">
-            </div>
-        </div>
-    </div>
-);
-
+        );
+    }
+}
 
 export { ChartsPage };
--- src/components/barchart.js
+++ src/components/barchart.js
@@ -1,14 +1,11 @@
 import React, { Component } from "react";
 import { BarChart } from "react-easy-chart";
-import { Chart, Axis, Series, Tooltip, Cursor, Line } from 'react-charts'
 
 class OurBarChart extends Component {
   render() {
-    const data=[
-    ];
     return (
       <div>
-        {/* <BarChart
+        <BarChart
           data={[
             { x: "A", y: 20 },
             { x: "B", y: 30, color: "#365382" },
@@ -18,22 +15,7 @@ class OurBarChart extends Component {
             { x: "F", y: 25 },
             { x: "G", y: 5, color: "orange" }
           ]}
-        /> */}
-        <Chart data={data}>
-          <Axis
-            primary
-            type="time"
-            position="bottom"
-          />
-          <Axis
-            type="linear"
-            position="left"
-          />
-          <Series type={Line} />
-          <Tooltip />
-          <Cursor primary />
-          <Cursor />
-        </Chart>
+        />
       </div>
     );
   }
